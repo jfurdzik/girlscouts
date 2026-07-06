73a4e621d71e12b7c228973ee46233270d144120
TDEV03M <tdev03m@dev.mtb.com>
2026-07-06 14:16:38 -0400

    Users added

---
+++ cookies/src/main/java/com/girlscouts/cookies/users/users.java
@@ -0,0 +1,36 @@
+package com.girlscouts.cookies.users;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String serviceUnit;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    public Users(){}
+
+    public Users(
+            String firstName,
+            String lastName,
+            String serviceUnit,
+            Role role) {
+        this.firstName = firstName;
+        this.lastname = lastName;
+        this.serviceUnit = serviceUnit;
+        this.role = role;
+    }
+}
---
+++ cookies/src/main/java/com/girlscouts/cookies/users/usersService.java
@@ -0,0 +1,55 @@
+package com.girlscouts.cookies;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public Users getUserById(Long id) {
+        return usersRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public Users createUser(Users user) {
+        return usersRepository.save(user);
+    }
+
+
+    public Users updateUser(Long id, Users updatedUser) {
+
+        Users user = getUserById(id);
+
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setRole(updatedUser.getRole());
+        user.setServiceUnit(updatedUser.getServiceUnit());
+        return usersRepository.save(user);
+    }
+
+
+    public void deleteUser(Long id) {
+        Users user = getUserById(id);
+        usersRepository.delete(user);
+    }
+
+
+    public List<Users> getUsersByRole(Role role) {
+        return usersRepository.findByRole(role);
+    }
+
+    public List<Users> getUsersByServiceUnit(String serviceUnit) {
+        return usersRepository.findByServiceUnit(serviceUnit);
+    }
+
+
+}
