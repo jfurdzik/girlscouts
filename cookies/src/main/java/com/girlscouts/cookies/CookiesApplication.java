f83d75359c874523e9628e176fde7ec2ecd7a09a
mrd3835 <mrd3835@rit.edu>
2026-07-06 12:54:02 -0400

    init pt2

--- cookies/src/main/java/com/girlscouts/cookies/CookiesApplication.java
+++ cookies/src/main/java/com/girlscouts/cookies/CookiesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.girlscouts.cookies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CookiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CookiesApplication.class, args);
 	}
 
 }
